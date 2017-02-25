3622d8ae58799b632b0239e0a890829b38c2df8d
Andris Azens <andris.azens@inbox.lv>
2017-02-25 06:03:56 +0200

    Module 2 tutorial

---
+++ dev234_async_js/Mod2/Tutorial/library.js
@@ -0,0 +1,23 @@
+(function(window){
+
+    function myLibrary(){
+
+        //execute code here
+
+        return {
+            searchProductById: searchProductById,
+            searchProductsByPrice: searchProductsByPrice,
+            searchProductsByType: searchProductsByType,
+            searchAllProducts: searchAllProducts
+        }
+
+        //function definitions go here
+
+
+    }
+
+    if(typeof(window.api) === 'undefined'){
+        window.api = myLibrary();
+    }
+
+})(window); 
