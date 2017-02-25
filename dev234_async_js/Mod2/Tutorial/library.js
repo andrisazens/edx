9ac5b9b35f8600ba8eea22ecf2a6cd720398758b
Andris Azens <andris.azens@inbox.lv>
2017-02-25 06:05:40 +0200

    Generate random catalog

--- dev234_async_js/Mod2/Tutorial/library.js
+++ dev234_async_js/Mod2/Tutorial/library.js
@@ -1,8 +1,10 @@
-(function(window){
+(function (window) {
 
-    function myLibrary(){
+    function myLibrary() {
 
         //execute code here
+
+        var catalog = createRandomCatalog(100);
 
         return {
             searchProductById: searchProductById,
@@ -13,10 +15,25 @@
 
         //function definitions go here
 
+        function createRandomProduct() {
+            var typeArray = ['Electronics', 'Book', 'Clothing', 'Food'];
+            var price = (Math.random() * 500).toFixed(2)
+            var type = typeArray[Math.floor(Math.random() * 4)];
 
+            return { price: price, type: type };
+        }
+
+        function createRandomCatalog(num) {
+            var catalog = [];
+            for (var i = 0; i < num; i++) {
+                var obj = createRandomProduct();
+                catalog.push({ id: i, price: obj.price, type: obj.type });
+            }
+            return catalog;
+        }
     }
 
-    if(typeof(window.api) === 'undefined'){
+    if (typeof (window.api) === 'undefined') {
         window.api = myLibrary();
     }
 
