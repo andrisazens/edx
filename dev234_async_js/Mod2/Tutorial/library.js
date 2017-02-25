9767b674490323f9f6dd2b23f8fe7e1c1d9e8d16
Andris Azens <andris.azens@inbox.lv>
2017-02-25 06:32:44 +0200

    Catalog methods

--- dev234_async_js/Mod2/Tutorial/library.js
+++ dev234_async_js/Mod2/Tutorial/library.js
@@ -15,6 +15,82 @@
 
         //function definitions go here
 
+        function searchProductsByPrice(price, diff) {
+
+            var promise = new Promise(function (resolve, reject) {
+                if (!isFinite(price)) {
+                    reject("Invalid price: " + price);
+                } else {
+                    var i = 0, products = [];
+                    setTimeout(function () {
+                        while (i < catalog.length) {
+                            if (Math.abs(catalog[i].price - price) < difference) {
+                                products.push(mapProduct(catalog[i]));
+                            }
+                            i++;
+                        }
+                        resolve(products);
+                    }, 1000);
+                }
+            });
+            return promise;
+        }
+
+        function searchProductsByType(type) {
+
+            var promise = new Promise(function (resolve, reject) {
+                var validTypes = ['Electronics', 'Book', 'Clothing', 'Food'];
+                if (!validTypes.includes(type)) {
+                    reject("Invalid Type: " + type)
+                } else {
+                    var i = 0, products = [];
+                    setTimeout(function () {
+                        while (i < catalog.length) {
+                            if (catalog[i].type == type) {
+                                products.push(mapProduct(catalog[i]));
+
+                            }
+                            i++;
+                        }
+                        resolve(products);
+                    }, 1000);
+                }
+            });
+            return promise;
+        }
+
+        function searchProductById(id) {
+
+            var promise = new Promise(function (resolve, reject) {
+                var i = 0;
+                setTimeout(function () {
+                    while (i < catalog.length) {
+                        if (catalog[i].id == id) {
+                            resolve(mapProduct(catalog[i]));
+                        }
+                        i++;
+                    }
+                    reject("Invalid ID: " + id);
+                }, 1000);
+            });
+            return promise;
+        }
+
+        function mapProduct(item) {
+            return { id: item.id, price: item.price, type: item.type }
+        }
+
+        function searchAllProducts() {
+            var promise = new Promise(function (resolve, reject) {
+
+                setTimeout(function () {
+                    resolve(catalog);
+                }, 1000);
+
+            });
+            return promise;
+        }
+
         function createRandomProduct() {
             var typeArray = ['Electronics', 'Book', 'Clothing', 'Food'];
             var price = (Math.random() * 500).toFixed(2)
