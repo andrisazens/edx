57637c0539b989b436bf458b101d3718f97d02dd
Andris Azens <andris.azens@inbox.lv>
2017-03-04 17:09:25 +0200

    Mod4 hw done

--- dev234_async_js/Mod4/Hw/index.js
+++ dev234_async_js/Mod4/Hw/index.js
@@ -1,19 +1,42 @@
 
-
+let starships = {};
 document.getElementById("button").addEventListener('click', function () {
-    // run(gen).catch(function (err) {
-    //     alert(err.message);
-    // });
+    run(compareGen).catch(function (err) {
+        alert(err.message);
+    });
 });
 load();
 
 function load() {
-    run(gen).catch(function (err) {
+    run(loadGen).catch(function (err) {
         alert(err.message);
     });
 }
 
-function* gen() {
+function* compareGen() {
+    let name1 = document.getElementById("starshipSelect1").value,
+        name2 = document.getElementById("starshipSelect2").value,
+        starship1 = starships[name1],
+        starship2 = starships[name2];
+
+    let props = ["name", "cost_in_credits", "max_atmosphering_speed", "cargo_capacity", "passengers"];
+    for (let prop of props) {
+        let elem1 = document.getElementById("ship1_" + prop), elem2 = document.getElementById("ship2_" + prop);
+        elem1.innerText = starship1[prop];
+        elem2.innerText = starship2[prop];
+        
+        if (prop != "name") {
+            let value1 = parseInt(starship1[prop], 10), value2 = parseInt(starship2[prop], 10);                        
+            if (value1 > value2) {
+                elem1.style.color = "red";
+            } else if (value2 > value1) {
+                elem2.style.color = "red";
+            }
+        }
+    }
+}
+
+function* loadGen() {
     //check if input is valid
     let starshipIds = [2, 75, 74, 65, 3, 59, 58, 63, 28, 29, 39, 10],
         starships1Select = document.getElementById("starshipSelect1"),
@@ -23,14 +46,14 @@ function* gen() {
         // fetch the starships
         var starshipResponse = yield fetch("http://swapi.co/api/starships/" + id);
         var starship = yield starshipResponse.json();
-        console.log(starship);
+        starships[starship.name] = starship;
 
         // populate the select's            
         let elem = document.createElement("option");
         elem.value = starship.name;
         elem.innerText = starship.name;
         starships1Select.appendChild(elem);
-        
+
         let elem2 = document.createElement("option");
         elem2.value = starship.name;
         elem2.innerText = starship.name;
